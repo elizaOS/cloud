e8008e213ce0ed42404323c76f4f4ce6616c2e04
Christopher Trimboli <cjft@trimboli.io>
2025-11-05 06:18:14 -0700

    fix my agents page

--- components/marketplace/index.ts
+++ components/marketplace/index.ts
@@ -1,13 +1,16 @@
-export { CharacterMarketplace } from "./character-marketplace";
+export { MyAgentsView } from "./character-marketplace";
+export { MyAgentsView as CharacterMarketplace } from "./character-marketplace"; // Backwards compatibility
 export { CharacterCard } from "./character-card";
 export { CharacterGrid } from "./character-grid";
 export { CategoryTabs } from "./category-tabs";
 export { FilterBar } from "./filter-bar";
-export { MarketplaceHeader } from "./marketplace-header";
+export { MyAgentsHeader } from "./marketplace-header";
+export { MyAgentsHeader as MarketplaceHeader } from "./marketplace-header"; // Backwards compatibility
 export { CharacterDetailsModal } from "./character-details-modal";
 export { EmptyStates } from "./empty-states";
 
 // Export hooks
-export { useMarketplaceFilters } from "./hooks/use-marketplace-filters";
+export { useMyAgentsFilters } from "./hooks/use-marketplace-filters";
+export { useMyAgentsFilters as useMarketplaceFilters } from "./hooks/use-marketplace-filters"; // Backwards compatibility
 export { useCharacterSearch } from "./hooks/use-character-search";
 export { useInfiniteCharacters } from "./hooks/use-infinite-characters";
