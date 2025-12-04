f6d92557d8b1c2f48dcd877473cea2b21c8a4358
samarth30 <samarth.gugnani30@gmail.com>
2025-12-04 21:40:44 +0530

    feat: adding model tiers + abstracting all models for a cleaner accessibility

---
+++ lib/models/index.ts
@@ -0,0 +1,11 @@
+export {
+  type ModelTier,
+  type ModelTierConfig,
+  MODEL_TIERS,
+  MODEL_TIER_LIST,
+  DEFAULT_MODEL_TIER,
+  getModelIdFromTier,
+  getTierFromModelId,
+  isValidModelTier,
+  STORAGE_KEY,
+} from "./model-tiers";
