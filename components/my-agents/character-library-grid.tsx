e0c876e8bd06dbf698b609aa43b586a2f35e724e
Rayan Meziani <rayanmeziani95@outlook.com>
2025-12-20 18:03:09 +0100

    ensure cards same

--- components/my-agents/character-library-grid.tsx
+++ components/my-agents/character-library-grid.tsx
@@ -34,7 +34,7 @@ export function CharacterLibraryGrid({
     <div
       className={
         viewMode === "grid"
-          ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
+          ? "grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4   gap-3"
           : "flex flex-col gap-3"
       }
     >
