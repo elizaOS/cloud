4e2f70b04ae42c6c33d5b5475bde1541087938ee
Shaw <shawmakesmagic@gmail.com>
2026-04-25 20:08:40 -0700

    build: add cloud sdk dist output

---
+++ packages/sdk/dist/index.js
@@ -0,0 +1,10 @@
+export {
+  createElizaCloudClient,
+  InsufficientCreditsError,
+  ElizaCloudHttpClient,
+  ElizaCloudClient,
+  CloudApiError,
+  CloudApiClient
+};
+
+//# debugId=D45F6C78647451B164756E2164756E21
