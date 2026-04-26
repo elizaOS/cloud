82187d9557a358dc12868232fff41af51bb4385d
Shaw <shawmakesmagic@gmail.com>
2026-04-25 20:08:40 -0700

    build: add cloud sdk dist output

---
+++ packages/sdk/dist/index.d.ts
@@ -0,0 +1,4 @@
+export { createElizaCloudClient, ElizaCloudClient } from "./client";
+export { CloudApiClient, CloudApiError, ElizaCloudHttpClient, InsufficientCreditsError, } from "./http";
+export type * from "./types";
+//# sourceMappingURL=index.d.ts.map
